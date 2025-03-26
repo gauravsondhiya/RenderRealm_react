9a238827e200308258fea27fbffbaa10fd238aeb
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-03-26 18:09:05 +0530

    sml app add

---
+++ playground/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  return <Wrapper>valuesdone </Wrapper>;
+}
+function Wrapper({ children }) {
+  return <div style={{ border: "solid 2px black" ,padding:"20px"}}>{children}</div>;
+}
+
+export default App;
---
+++ todoapp/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function App(){
+
+  function clicker(){
+    
+  }
+
+ return (
+   <>
+    <h1>Todo app</h1>
+      <input type="text"/>
+      <button onClick={clicker}>Add Todo</button>
+  </>
+ )
+}
+
+export default App;
+
