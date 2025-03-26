bf16e08ca196fc50d41252237e45f840f0bba0c8
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-03-26 19:24:14 +0530

    sml update

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
+function App() {
+  let [invalue, setinvalue] = useState("");
 
-function App(){
+  let [outvalue, setOutvalue] = useState([]);
 
-  function clicker(){
-    
+  function clicker() {
+    if (!invalue == "") {
+      setOutvalue([...outvalue, invalue]);
+      setinvalue("");
+    }
   }
-
- return (
-   <>
-    <h1>Todo app</h1>
-      <input type="text"/>
+  return (
+    <>
+      <h1>Todo app</h1>
+      <input
+        type="text"
+        value={invalue}
+        onChange={(e) => setinvalue(e.target.value)}
+      />
       <button onClick={clicker}>Add Todo</button>
-  </>
- )
+      <div>
+        {outvalue.map((e) => (
+          <div key={Math.random()} style={{ display: "flex", gap: "30px" }}>
+            <p>{e}</p>
+            <button
+              onClick={() => {
+                let ans = outvalue.filter((v)=>{return e!=v })
+                setOutvalue(ans);
+              }}
+            >
+              Done
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
-
