eb0d6e5ff81390216b3ac0a80db7d5a11940d11f
Gaurav Sondhiya <gauravsondhiya1503@gmail.com>
2025-09-07 10:05:31 +0530

    codeupdate

--- reduxplay/src/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  todoslicereducer  from '../Features/TodoSlice'
-
-export const store =configureStore({
-    reducer:{
-      todo:todoslicereducer
-    }
-})
